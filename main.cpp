abeb2f33100313e4daf9d2242214a9a738041c7d
Anit0621 <valen02056@gmail.com>
2025-07-19 22:29:26 -0500

    Ajustes finales

--- main.cpp
+++ main.cpp
@@ -1,3 +1,24 @@
+/*
+CASO DE ESTUDIO - PUNTO DE AVATAR CPU
+Profesor: Victor Bucheli
+Estudiante: Valentina Montezuma Gonzalez
+Codigo: 202242058
+Correo: valentina.montezuma@correounivalle.edu.co
+
+
+Descripcion de caso:
+
+Se implementó en el repositorio de GitHub una actualización que añade dos avatares al juego: el avatar principal controlado 
+por el jugador usando WASD, que puede ganar alcanzando la salida o perder cayendo en abismos, de este se hizo la implementacion 
+que estaba pendiente y se mostro en la guia del caso de estudio. Por otra parte se hizo tambien la implementacion del AvatarCPU 
+que se mueve automáticamente en direcciones aleatorias por el tablero sin afectar la jugabilidad principal, se hizo aplicando lo
+de la guia y adicional a esto modificandola para que cumpliera con el polimorfismo segun las guias vistas. 
+Mientras el jugador se enfoca en su objetivo, el AvatarCPU se desplaza de forma independiente añadiendo dinamismo visual al juego, 
+manteniendo intactas las mecánicas originales donde solo el avatar controlado determina el resultado final.
+
+*/
+
+
 #include <iostream>
 #include "Tablero.h"
 #include "Juego.h"
